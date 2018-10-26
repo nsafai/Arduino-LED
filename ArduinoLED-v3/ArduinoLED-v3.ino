4794a880abb7b4ab3a4e726d822970d37ccf362c
Antoine Barthelet <antoine@flyrlabs.com>
2018-10-25 19:38:24 -0700

    columns light up but don't turn off

--- ArduinoLED-v3/ArduinoLED-v3.ino
+++ ArduinoLED-v3/ArduinoLED-v3.ino
@@ -23,25 +23,30 @@ void setup() {
 }
  
 void loop() {
-  for(int row = 0; row < rows; row++){
-    dot = row;
+    int led_array[] = {0, 1, 2, 3, 4, 5, 6, 7};
     for(int col = 0; col < columns; col++) {
+        int *old_leds = led_array;
         Serial.print("Column: "); Serial.print(col); Serial.print("\n");
         Serial.print("LED: "); Serial.print(dot); Serial.print("\n");
-        
-        leds[dot].setHue(dot);
-        FastLED.show();
-        // clear this led for the next time around the loop  
-        delay(10);
-        leds[dot] = CRGB::Black;
-        if(col%2==0){
+        for(int led = 0; led < sizeof(led_array); led++){
+          leds[led_array[led]].setHue(led_array[led]);
+          if(col%2==0){
           // For every row down from the first, two LEDs less to travel to next led same row due to snake pattern
-          dot+=15+row*(-2);
+          led_array[led]+=15+led*(-2);
           }
          else{
           // For every row down from the first, two LEDs more to travel to next led same row due to snake pattern
-          dot+=1+row*2;
+          led_array[led]+=1+led*2;
           }
+        FastLED.show();
+        // clear this led for the next time around the loop  
+        delay(10);
+        }
+        for(int led = 0; led < sizeof(led_array); led++){
+          leds[old_leds[led]] = CRGB::Black;
+        }
+        
+        
+        
     }
-  }
 }
